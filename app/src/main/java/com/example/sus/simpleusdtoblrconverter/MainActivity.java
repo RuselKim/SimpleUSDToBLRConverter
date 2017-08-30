d1eaaebf6cb4be327a1d6469c06d702c7071fd11
RuselKim <sus.metalist@gmail.com>
2017-08-30 18:59:01 +0300

    Closed #1 Create an App to convert USD to BYR and BYR to USD

---
+++ app/src/main/java/com/example/sus/simpleusdtoblrconverter/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.sus.simpleusdtoblrconverter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public float convertUsdToByr(float usd) {
+        return (float) (usd * 1.932);
+    }
+
+    public float convertByrToUsd(float byr) {
+        return (float) (byr / 1.932);
+    }
+
+    public void onClickConvert(View view) {
+
+        EditText inputEditText = (EditText) findViewById(R.id.editText_enterPrice);
+        RadioButton radioButtonUsdToByr = (RadioButton) findViewById(R.id.rButton_usdToByr);
+        RadioButton radioButtonByrToUsd = (RadioButton) findViewById(R.id.rButton_byrToUsd);
+        TextView resultTextView = (TextView) findViewById(R.id.textView_result);
+
+        if (inputEditText.getText().length() == 0) {
+            Toast.makeText(getApplicationContext(), R.string.toast_emptiInput,
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        float inputValue = Float.parseFloat(inputEditText.getText().toString());
+
+        if (radioButtonUsdToByr.isChecked()) {
+            resultTextView.setText(String.valueOf(convertUsdToByr(inputValue)));
+        } else if (radioButtonByrToUsd.isChecked()) {
+            resultTextView.setText(String.valueOf(convertByrToUsd(inputValue)));
+        }
+
+    }
+}
