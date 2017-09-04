3f33b6c7a9f7ed6cbf43b988c44078ac72328589
RuselKim <sus.metalist@gmail.com>
2017-09-04 21:04:42 +0300

    Closed #2 Connect app to the nbrb.by

--- app/src/main/java/com/example/sus/simpleusdtoblrconverter/MainActivity.java
+++ app/src/main/java/com/example/sus/simpleusdtoblrconverter/MainActivity.java
@@ -1,25 +1,36 @@
 package com.example.sus.simpleusdtoblrconverter;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
-    double rate;
+    private double rate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ParseActualRates parseActualRates = new ParseActualRates();
-        parseActualRates.execute();
-        rate = parseActualRates.actualRate;
     }
+
+
 
     public float convertUsdToByr(float usd) {
         return (float) (usd * rate);
@@ -27,6 +38,12 @@ public class MainActivity extends AppCompatActivity {
 
     public float convertByrToUsd(float byr) {
         return (float) (byr / rate);
+    }
+
+    public void onClickGetRates(View view) {
+        ParseActualRates parseActualRates = new ParseActualRates();
+        parseActualRates.execute();
+
     }
 
     public void onClickConvert(View view) {
@@ -50,5 +67,60 @@ public class MainActivity extends AppCompatActivity {
             resultTextView.setText(String.valueOf(convertByrToUsd(inputValue)));
         }
 
+    }
+
+    class ParseActualRates extends AsyncTask<Void,Void,Double> {
+
+        HttpURLConnection urlConnection;
+        BufferedReader reader;
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.rates_progressBar);
+        Button buttonGetRates = (Button) findViewById(R.id.button_getRates);
+        protected double actualRate;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+            buttonGetRates.setEnabled(false);
+        }
+
+        @Override
+        protected Double doInBackground(Void... voids) {
+            try {
+                URL url = new URL("http://www.nbrb.by/API/ExRates/Rates/USD?ParamMode=2");
+                urlConnection = (HttpURLConnection)url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line=reader.readLine())!= null){
+                    buffer.append(line);
+                }
+
+                JSONObject jsonObject = new JSONObject(buffer.toString());
+                actualRate = jsonObject.getDouble("Cur_OfficialRate");
+
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return actualRate;
+        }
+
+        @Override
+        protected void onPostExecute(Double s) {
+            super.onPostExecute(s);
+            progressBar.setVisibility(View.INVISIBLE);
+            Toast.makeText(MainActivity.this, "Actual rates = " + s, Toast.LENGTH_SHORT).show();
+            buttonGetRates.setEnabled(true);
+            rate = s;
+
+        }
     }
 }
